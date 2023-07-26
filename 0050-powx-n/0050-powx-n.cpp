8ab659f75d154fa6e36a666527bf6d87bde30382
vivekmishra9631 <114434748+vivekmishra9631@users.noreply.github.com>
2023-07-26 17:05:25 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (91.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n==0) return 1;
+        double ans=1;
+        long long nn= n;
+        if(nn<0)  nn=-1*nn;
+        
+        while(nn >0){
+            if(nn %2==0){
+                x*=x;
+                nn/=2;
+            } 
+            
+            else {
+                ans=ans*x;
+                nn--;
+            }
+        }
+        if(n<0) return 1/ans;
+        
+       return ans; 
+    }
+};
