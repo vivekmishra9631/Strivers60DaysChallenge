f605c6ab482195cea25d773bcf8554e9ce793b72
vivekmishra9631 <114434748+vivekmishra9631@users.noreply.github.com>
2023-08-08 13:08:51 +0530

    Time: 2 ms (62.56%), Space: 7.6 MB (81.22%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,12 +1,37 @@
 class Solution {
+    
+private:
+    void permutation(vector<int>& nums,int vis[],vector<int>&temp,vector<vector<int>>&ans,int n){
+            if(temp.size()==n){
+                ans.push_back(temp); 
+                return;
+            }
+           
+       
+        for (int i = 0; i <n; i++) {
+        if (!vis[i]) {
+          temp.push_back(nums[i]);
+          vis[i] = 1;
+          permutation(nums,vis,temp,ans,n);
+          vis[i] = 0;
+          temp.pop_back();
+        }
+      }
+        
+        
+        
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        do{
-            ans.push_back(nums);
+        vector<int>temp;
+        int n=nums.size();
+        int vis[n];
+        for(int i=0;i<n;i++){
+            vis[i]=0;
         }
-        while (next_permutation(nums.begin(), nums.end()));
+        sort(nums.begin(),nums.end());
+        permutation(nums,vis,temp,ans,n);
         return ans;
     }
 };
