54fd7637037f9b417c2a730f289cfaf18998c25d
vivekmishra9631 <114434748+vivekmishra9631@users.noreply.github.com>
2023-08-17 01:36:55 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (9.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(auto it:s){
-            if(it=='(' || it=='{' || it=='['){
-                st.push(it);
-            }
-            
-            else if((it==')' || it==']' || it=='}') && st.empty()) return false ;
-            else if(!st.empty()){
-            if(it ==')'){
-              if (st.top()!='(' )
-                return false; 
-                  
-                else st.pop();
-            }  
-                
-            else if(it =='}'){
-              if (st.top()!='{')
-                return false; 
-                  
-                else st.pop();
-            
-        }
-          else if(it ==']'){
-              if (st.top()!='[')
-                return false; 
-                  
-                else st.pop();
-            
-        }
+        stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+                else return false;
             }
         }
-        if(!st.empty()) return false;
-        return true;
-        
+        return st.empty(); 
     }
+        
+    
 };
